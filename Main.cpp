e9e50d9ebb69952c0f969441d62a5d01e376a5ae
Vincent Villani <villanivincent@gmail.com>
2016-05-18 12:52:31 +1000

    Updates notes

--- Main.cpp
+++ Main.cpp
@@ -10,6 +10,7 @@
 #include "Header/RFIMStar.h"
 #include "Header/UnitTests.h"
 
+//TODO: Use a small part of real data and go through everything step by step - try to figure out whats going on
 //TODO: Figure out how to correctly project and deproject
 //TODO: What if the values go above or below a the range that the bits can represent?
 //TODO: Calculate how many dimensions to reduce at run-time, rather than a hardcoded 1
@@ -21,21 +22,21 @@ int main()
 {
 
 	//Unit tests
-	RunAllUnitTests();
+	//RunAllUnitTests();
 
 
-/*
-	uint32_t beamNum = 13;
+
+	uint32_t beamNum = 2;
 	uint32_t rawDataBlockNum = 3;
 	uint32_t numberOfWorkerThreads = 10;
 	uint32_t windowSize = 15625;
-	uint32_t dimensionsToReduce = 2;
+	uint32_t dimensionsToReduce = 1;
 
 	std::string inputFilenamePrefix = "/lustre/projects/p002_swin/surveys/SUPERB/2016-01-05-12:07:06/";
 	std::string inputFilenamePostfix = "/2016-01-05-12:07:06.fil";
 
 	std::string outputFilenamePrefix = "/lustre/projects/p002_swin/vvillani/";
-	std::string outputFilenamePostfix = "a2-15625W.fil";
+	std::string outputFilenamePostfix = "test.fil";
 
 	RFIMConfiguration configuration = RFIMConfiguration(numberOfWorkerThreads, windowSize, beamNum, dimensionsToReduce, rawDataBlockNum,
 			inputFilenamePrefix, inputFilenamePostfix,
@@ -46,7 +47,7 @@ int main()
 
 	std::cout << "Done!" << std::endl;
 
-	*/
+
 
 
 	return 0;
